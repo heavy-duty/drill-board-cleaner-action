96714783e2c1a9987418417e3a753414dfbb5690
Daniel Marin <danielarturomt@gmail.com>
2022-05-15 21:31:32 +0100

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# drill-board-cleaner-action
+# Drill Board Cleaner Action
+
+This action goes through each of the issues with the `drill:bounty:closed` label, checks if the bounty was claimed in which case removes the label.
+
+## Inputs
+
+## `program-id`
+
+**Required** The programId of the Drill Program.
+
+## Outputs
+
+## `result`
+
+The result of running the action.
+
+## Example usage
+
+uses: actions/drill-vault-updater-action@v1.1
+with:
+program-id: '2fg324Gf51Nrp1jzxXETvJmXPEiHz9ybNY1r575MtijW'
---
+++ index.js
@@ -0,0 +1,82 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { getAccount } = require("@solana/spl-token");
+const { Connection, PublicKey } = require("@solana/web3.js");
+const BN = require("bn.js");
+
+async function run() {
+  try {
+    const programId = core.getInput("program-id");
+    const githubRepository = core.getInput("github-repository");
+    const rpcEndpoint = core.getInput("rpc-endpoint");
+    const token = core.getInput("token");
+
+    const [owner, repoName] = githubRepository.split("/");
+    const connection = new Connection(rpcEndpoint);
+    const octokit = github.getOctokit(token);
+
+    const { data: repository } = await octokit.rest.repos.get({
+      repo: repoName,
+      owner,
+    });
+
+    const [boardPublicKey] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("board", "utf8"),
+        new BN(repository.id).toArrayLike(Buffer, "le", 4),
+      ],
+      new PublicKey(programId)
+    );
+
+    // get all issues with a bounty enabled
+    const { data: issuesForRepo } = await octokit.rest.issues.listForRepo({
+      repo: repoName,
+      owner,
+      labels: "drill:bounty:closed",
+      state: "closed",
+    });
+  
+    issuesForRepo.forEach(async (issue) => {
+      // find bounty enabled comment
+      const [bountyPublicKey] = await PublicKey.findProgramAddress(
+        [
+          Buffer.from("bounty", "utf8"),
+          boardPublicKey.toBuffer(),
+          new BN(issue.number).toArrayLike(Buffer, "le", 4),
+        ],
+        new PublicKey(programId)
+      );
+      const [bountyVaultPublicKey] = await PublicKey.findProgramAddress(
+        [Buffer.from("bounty_vault", "utf8"), bountyPublicKey.toBuffer()],
+        new PublicKey(programId)
+      );
+
+      let account;
+      let error;
+
+      try {
+        account = await getAccount(
+          connection,
+          bountyVaultPublicKey
+        );
+      } catch(err) {
+        error = err;
+      }
+
+      if (error !== undefined) {
+        await octokit.issues.removeLabel({
+          comment_id: bountyEnabledComment.id,
+          owner,
+          repo: repoName,
+          name: 'drill:bounty:closed'
+        });
+      }
+    });
+
+    core.setOutput("result", true);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
